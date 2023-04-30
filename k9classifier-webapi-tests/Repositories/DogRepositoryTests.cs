7660d6d3918dc4876ef40976b5fc4014b4386ba2
maotterson <otterson.2@gmail.com>
2023-04-30 00:30:30 -0400

    add dog repository unit test

---
+++ k9classifier-webapi-tests/Repositories/DogRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using k9classifier_webapi.Repositories;
+
+namespace k9classifier_webapi_tests.Repositories;
+public class DogRepositoryTests
+{
+    [Fact]
+    public void GetDogById_ReturnsDog_WhenDogExists()
+    {
+        // Arrange
+        var repository = new DogRepository();
+
+        // Act
+        var dog = repository.GetDogById(1);
+
+        // Assert
+        Assert.NotNull(dog);
+        Assert.Equal("Fido", dog.Name);
+        Assert.Equal("Husky", dog.Breed);
+    }
+}
