a68be6edc88e44c2cdfba3475f3c7a0ad3075030
maotterson <otterson.2@gmail.com>
2023-04-30 00:19:57 -0400

    add simple dogrepository implementation for crud operations

--- k9classifier-webapi/Repositories/DogRepository.cs
+++ k9classifier-webapi/Repositories/DogRepository.cs
@@ -1,9 +1,42 @@
-﻿namespace k9classifier_webapi.Repositories;
+﻿using k9classifier_webapi.Entities;
+
+namespace k9classifier_webapi.Repositories;
 
 public class DogRepository : IDogRepository
 {
-    public IEnumerable<string> GetDogNames()
+    private readonly List<Dog> _dogs = new List<Dog>();
+
+    public Dog GetDogById(int id)
     {
-        throw new NotImplementedException();
+        return _dogs.FirstOrDefault(dog => dog.Id == id);
+    }
+
+    public IEnumerable<Dog> GetAllDogs()
+    {
+        return _dogs;
+    }
+
+    public void AddDog(Dog dog)
+    {
+        _dogs.Add(dog);
+    }
+
+    public void UpdateDog(Dog dog)
+    {
+        var existingDog = GetDogById(dog.Id);
+        if (existingDog != null)
+        {
+            _dogs.Remove(existingDog);
+            _dogs.Add(dog);
+        }
+    }
+
+    public void DeleteDog(int id)
+    {
+        var existingDog = GetDogById(id);
+        if (existingDog != null)
+        {
+            _dogs.Remove(existingDog);
+        }
     }
 }
--- k9classifier-webapi/Repositories/IDogRepository.cs
+++ k9classifier-webapi/Repositories/IDogRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace k9classifier_webapi.Repositories;
+﻿using k9classifier_webapi.Entities;
+
+namespace k9classifier_webapi.Repositories;
 
 public interface IDogRepository
 {
-    IEnumerable<string> GetDogNames();
+    Dog GetDogById(int id);
+    IEnumerable<Dog> GetAllDogs();
+    void AddDog(Dog dog);
+    void UpdateDog(Dog dog);
+    void DeleteDog(int id);
 }
